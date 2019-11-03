80dd8d9f67acc7c558bcd1419385fea9a3ded525
Spectra <pbldmngz@users.noreply.github.com>
2019-11-02 17:35:31 -0700

    tt

--- create_random_data.py
+++ create_random_data.py
@@ -1,5 +1,6 @@
 import generador as g
 import crud
+import random
 import re
 
 def get_id(table, col):
@@ -7,17 +8,19 @@ def get_id(table, col):
     .ver("max({})".format(col), table, "1 = 1")))) + 1
     #Obtener el index donde debe ser colocado
 
+def insertar_materia(alumno):
+    s = "materia_alumno" if alumno else "materia_profesor"
+    for i in range(0, 7):
+        if random.randint(0,1) == 1: crear(s, "", i)
+
 def insertar_profesor():
     p = g.generar_persona(get_id("profesor", "id_empleado") , False)
     crud.crear("profesor", "", "{},'{}','{}','{}','{}','{}'"
     .format(p[0],p[5],p[4],p[1],p[2],p[3]))
+    insertar_materia(False)
 
 def insertar_alumno():
     a = g.generar_persona(get_id("alumno", "id_alumno"), True)
     crud.crear("alumno", "", "{},'{}','{}','{}','{}','{}'"
     .format(a[0],a[5],a[1],a[2],a[3],a[6]))
-
-#def insertar materia_profesor y materia_alumno
-
-insertar_alumno()
-#Recuerda que para consola que podrá usar el usuario puedes utilizar re para evitar sql injection
+    insertar_materia(True)
